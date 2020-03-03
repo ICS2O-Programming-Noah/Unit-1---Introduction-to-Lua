ac9ddc85c5589546a98129585c8ebc879d3d26d4
Noah Sabbagh <noah.sabbagh@ocsbstudent.ca>
2020-03-03 13:52:22 -0500

    Added Touch and Drag

---
+++ TouchAndDragNoah/main.lua
@@ -0,0 +1,12 @@
+-----------------------------------------------------------------------------------------
+-- Title: TouchAndDrag
+-- Name: Noah Sabbagh
+-- Course: ICS2O
+-- This program...
+-----------------------------------------------------------------------------------------
+
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- create local variables. I am still trying to get the x-value to be set properly
+local backgorundImage = display.newImageRect("Images/background.png", 2048, 1536)
