b38af2a6dc34f94a9d6f237997f08fb8b5f7e7c9
Noah Sabbagh <61517057+s462947@users.noreply.github.com>
2020-04-20 11:06:57 -0400

    AddedWhackAMole

--- WhackAMoleNoah/main.lua
+++ WhackAMoleNoah/main.lua
@@ -1,90 +1,108 @@
------------------------------------------------------------------------------------------
--- Title: Whack A Mole
--- main.lua
--- Created by: Noah Sabbagh
+----------------------------------------------------------------------------------------
+-- Title: WhackAMoleMalcolm
+-- Name: Malcolm Cantin
 -- Course: ICS2O
 -- This program places a random object on the screen. If the user clicks on it in time,
 -- the score increases by 1.
------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------
 
--- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
------------------------------------------------------------------------------------------
--- LOCAL VARIABLES
------------------------------------------------------------------------------------------
-
-local bkg
-local alien
-local points = 0
-local pointsText
-
------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------
 -- OBJECT CREATION
------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------
 
-bkg = display.newRect( 0, 0, display.contentWidth, display.contentHeight )
-bkg:setFillColor(102/255, 159/255, 255)
--- set the position
-bkg.anchorX = 0
-bkg.anchorY = 0
-bkg.x = 0
-bkg.y = 0
+-- Create the background 
+    local bkg = display.newRect(0, 0, display.contentWidth, display.contentHeight)
+    bkg:setFillColor(33/255, 15/255, 108/255)
 
--- create the aliens
-alien = display.newImage( "Images/alien.png", 0, 0)
--- set the position
-alien.x = display.contentCenterX
-alien.y = display.contentCenterY
--- scale the image to be 1/3 of its original size
-alien:scale(0.3, 0.3)
--- make the alien invisible
-alien.alpha = 0
+    -- Set the position of the background
+    bkg.anchorX = 0
+    bkg.anchorY = 0
+    bkg.x = 0
+    bkg.y = 0
 
--- add text object for the score
-pointsText = display.newText("Points = " .. points, display.contentWidth*3/8, display.contentHeight*1/6, nil, 50)
-pointsText:setTextColor(199/255, 41/255, 205/255)
-pointsText.isVisible = true
+-- Creating Waldo
+local alien = display.newImage("Images/alien.png", 0, 0)
 
------------------------------------------------------------------------------------------
+    -- Set the position of the Waldo
+    alien.x = display.contentCenterX
+    alien.y = display.contentCenterY
+
+    alien:scale(2/5, 2/5)
+
+    alien.isVisible = false
+
+-- Variable to keep track of score
+local score = 0
+
+-- Local text object to display score
+local scoreText = display.newText("Score = " .. score .. "", 900, 725, nil, 60)
+scoreText:setFillColor(95/255, 15/255, 3/255)
+
+----------------------------------------------------------------------------------------
 -- FUNCTIONS
------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------
+
+-- This function makes Waldo appear in a random (x,y) position on the screen
+-- before calling the the Hide funtion
 function PopUp()
 
-	-- Choose a random position on the screen bewteen 0 and the size of the screen
-	alien.x = math.random( 0, display.contentWidth)
-	alien.y = math.random( 0, display.contentHeight)
-	alien.isVisible = true
-	Hide()
-	timer.performWithDelay(500, Hide)
+    -- Choosing random position on the screen between 0 and the size of the screen
+    alien.x = math.random(0, display.contentWidth)
+    alien.y = math.random(0, display.contentHeight)
+
+    alien.isVisible = true
+
+    timer.performWithDelay(800, Hide)
+
 end
 
+-- This function calls the PopUp function after 3 seconds
 function PopUpDelay()
-	timer.performWithDelay(3000, PopUp)
+
+    timer.performWithDelay(3000, PopUp)
+
 end
 
+-- This function makes the Waldo invisible and then calls the PopUpDelay function
 function Hide()
-	-- change the visibility of the alien
-	alien.isVisible = false
-	PopUpDelay()
+
+    -- Change the visibility
+    alien.isVisible = false
+
+    PopUpDelay()
+
 end
 
+-- This function starts the game
 function GameStart()
-	PopUpDelay()
+
+    PopUpDelay()
+
 end
 
+-- This function increments the score only if the Waldo is clicked. It then displays the
+-- new score.
 function Whacked(event)
-	-- if touch phase just started
-	if (event.phase == "began") then
-		points = points + 1
-		pointsText.text = "Points = " .. points
-	end
+
+    -- If touch phase just started
+    if (event.phase == "began") then
+
+        score = score + 1
+        scoreText.text = "Score = " .. score .. ""
+    end
+
 end
 
---------------------------------------- Event Listeners --------------------------------------
--- I add the event listener to the aliens so that if the alien is touched, the Whacked function
--- is called
-alien:addEventListener( "touch", Whacked )
+----------------------------------------------------------------------------------------
+-- EVENT LISTENERS
+----------------------------------------------------------------------------------------
+-- I added the event listener to Waldo so that if Waldo is touched, the Whacked
+-- function is called
+alien:addEventListener( "touch", Whacked)
 
----------------------------------------- Start the Game ---------------------------------
+----------------------------------------------------------------------------------------
+-- START THE GAME
+----------------------------------------------------------------------------------------
 GameStart()
